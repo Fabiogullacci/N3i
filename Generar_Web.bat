202e018654bf6b618c4f9efad0f831fabba16a0b
Fabiogullacci <fabiogullacci@gmail.com>
2026-06-12 10:25:10 -0300

    Update Generar_Web.bat to clean up old compiled assets before copying

--- Generar_Web.bat
+++ Generar_Web.bat
@@ -6,6 +6,24 @@ echo.
 echo Compilando la pagina (esto tomara un par de segundos)...
 call npm run build
 echo.
+echo Limpiando archivos compilados anteriores en la raiz...
+if exist _astro rmdir /S /Q _astro
+if exist assets rmdir /S /Q assets
+if exist auditoria rmdir /S /Q auditoria
+if exist empleo rmdir /S /Q empleo
+if exist en rmdir /S /Q en
+if exist gracias rmdir /S /Q gracias
+if exist herramientas rmdir /S /Q herramientas
+if exist obras rmdir /S /Q obras
+if exist portal rmdir /S /Q portal
+
+if exist index.html del /Q index.html
+if exist robots.txt del /Q robots.txt
+if exist sitemap-index.xml del /Q sitemap-index.xml
+if exist sitemap-0.xml del /Q sitemap-0.xml
+if exist sitemap.xml del /Q sitemap.xml
+
+echo.
 echo Copiando archivos compilados a la raiz de forma automatica...
 xcopy /E /Y /I dist\* .
 echo.
